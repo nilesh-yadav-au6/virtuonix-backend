3d612071a58ae8e96162abe15af106841442a035
Nilesh Yadav <nileshyadav@Nileshs-MacBook-Pro.local>
2024-10-23 22:26:48 +0530

    added whitelist frontend url to env

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend URL
+    origin: process.env.FRONTEND_URL, // Replace with your frontend URL
     credentials: true,
   })
 );
