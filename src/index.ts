93154742921ffb0b78a843f2e59f73a54816f70e
Nilesh Yadav <nileshyadav@Nileshs-MacBook-Pro.local>
2024-10-23 23:47:35 +0530

    urll correction

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000/", // Replace with your frontend URL
+    origin: "http://localhost:3000", // Replace with your frontend URL
     credentials: true,
   })
 );
